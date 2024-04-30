08b9934e2630c69e9f00c5db0a4b18318c6ea11c
GG <geeonmn072020@gmail.com>
2024-04-30 11:04:02 +0100

    add view

--- APICombine/APICombine/View/UsersListView.swift
+++ APICombine/APICombine/View/UsersListView.swift
@@ -2,8 +2,32 @@
 import SwiftUI
 
 struct UsersListView: View {
+    @ObservedObject var viewModel = UserListViewModel(networkManager: NetworkManager())
+    @State var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20) {
+                Button("Cancel the API request") {
+                    viewModel.cancelAPICall()
+                }
+                List(viewModel.filteredUserList) { user in
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("UserName: \(user.username)")
+                        Text("Name: \(user.name)")
+                        Text("Email: \(user.email)")
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .onChange(of: searchText, perform: { newValue in
+                print(newValue)
+                viewModel.filterUsersList(searchText: newValue)
+            })
+            .onAppear {
+                viewModel.fetchUsersFromAPI(urlString: APIEndPoint.UserEndPoint)
+            }
+        }
     }
 }
 
